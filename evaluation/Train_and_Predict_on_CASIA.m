d24b1a61c481ccb004c05e4af1f51956d648bb1d
BetaSK <57062334+BetaSK@users.noreply.github.com>
2019-10-27 21:26:24 +0800

    Update Train_and_Predict_on_CASIA.m

--- evaluation/Train_and_Predict_on_CASIA.m
+++ evaluation/Train_and_Predict_on_CASIA.m
@@ -1,1 +1,9 @@
 
+function Result = Train_and_Predict_on_CASIA(X_train,Y_train,X_test,Y_test,classifier) 
+    
+    if strcmp(classifier, 'SVM')
+        Result = Train_and_Predict_on_CASIA_SVM(X_train,Y_train,X_test,Y_test);
+    elseif  strcmp(classifier, 'ProCRC')     
+        Result = Train_and_Predict_on_CASIA_ProCRC(X_train,Y_train,X_test,Y_test);
+    end
+end
