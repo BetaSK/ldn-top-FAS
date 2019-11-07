cc44446db3906053cf43b5582a7f5f65fdd04604
BetaSK <57062334+BetaSK@users.noreply.github.com>
2019-11-07 22:17:47 +0800

    Update setup.m

--- setup.m
+++ setup.m
@@ -3,18 +3,11 @@ function options = setup()
 add_all_path();
 
 %% Dataset and Protocol Selection
-options.dataset = 'CASIA'; % 'CASIA', 'Replay-Attack', 'MSU', 'UVAD'
-switch options.dataset
-    case 'CASIA'
-        options.dataPath = 'E:\MATLAB\MATLAB\R2016b\bin\SK-LDN-TOP Paper Code\data preparation\dataset\';
-    case 'Replay-Attack'
-        options.dataPath = 'E:\MATLAB\MATLAB\R2016b\bin\SK-LDN-TOP Paper Code\data preparation\dataset\replay-attack\';
-    case 'UVAD'
-        options.dataPath = 'H:\UVAD\';
-end
+options.dataset = 'CASIA'; % 'CASIA', 'Replay-Attack', 'UVAD'
+options.dataPath = '.\data\dataset';
 
 %% Face Detector Selection
-options.face_detector = setup_MTCNN();
+options.face_detector = setup_mtcnn();
 
 %% Face Image Normalization
 options.imageSize = [64,64];
@@ -29,10 +22,6 @@ options.sigma = [1.0,1.5,2.0];
 % block division
 options.blockSize_xy = [16,16];
 options.overlap_xy = [0,0];
-options.blockSize_xt = [options.imageSize(1), options.numFrame];
-options.overlap_xt = [0,0]; 
-options.blockSize_ty = [options.numFrame,options.imageSize(2)];
-options.overlap_ty = [0,0];
 
 %% classifier
 options.classifier = 'ProCRC';
